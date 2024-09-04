ecf983dba92086a6568338d0a904603fbe78e764
Ahmed Ali Hassan <143716708+Aah353@users.noreply.github.com>
2024-09-04 10:33:09 +0400

    Add files via upload

---
+++ hello_you.py
@@ -0,0 +1,9 @@
+def hello_world():   #assigning the function
+    print("Hello, world") #calling a print function
+
+def hello_you():
+   name = input("what is ur name: ")  #calling an input function
+   print("Hello!", name)
+   
+hello_world()  #calling the def function so the code runs
+hello_you()
