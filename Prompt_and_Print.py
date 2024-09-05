b149e4ddb8239f51658b03928374fab4f187c76b
Ahmed Ali Hassan <143716708+Aah353@users.noreply.github.com>
2024-09-05 16:38:43 +0400

    Add files via upload

---
+++ Prompt_and_Print.py
@@ -0,0 +1,12 @@
+def prompt_and_print():
+    number1 = int(input("Enter the first number: "))
+    number2 = int(input("Enter the second number: "))
+    print(int(number1 + number2))
+    print(int(number1 - number2))
+    print(int(number1 * number2))
+    print(int(number1 / number2))
+
+
+
+prompt_and_print()
+
