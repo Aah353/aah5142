7b43cdecf82472762a5c0e7383c3ec4a5c1ab0f2
Ahmed Ali Hassan <143716708+Aah353@users.noreply.github.com>
2024-09-06 09:38:15 +0400

    Add files via upload

---
+++ expressions_practice.py
@@ -0,0 +1,12 @@
+def expressions_practice():
+    number1 = int(input("Enter the first number: ")) #asking for the input value
+    number2 = int(input("Enter the second number: ")) #asking for the input value
+    print(int(number1 + number2)) #addition
+    print(int(number1 - number2)) #Subtraction
+    print(int(number1 * number2)) #Multiplication
+    print(int(number1 / number2)) #Division
+    print(int(number1 // number2)) #Floor Division
+    print(int(number1 ** number2)) #Exponent
+    print(int(number1**number2-number1+number2/number1)) # A mix of 4 different arithmentic functions
+
+expressions_practice()
