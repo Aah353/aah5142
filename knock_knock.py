ca097f312c1062c028228283537e32681e539aad
Ahmed Ali Hassan <143716708+Aah353@users.noreply.github.com>
2024-09-06 10:20:53 +0400

    Add files via upload

---
+++ knock_knock.py
@@ -0,0 +1,12 @@
+def joke(): 
+    part1 = input("Knock Knock: ")
+    print(part1)
+    part2 = input("whose there who? ")
+    print(part2)
+    part3 = input("old lade who? ")
+    print(part3)
+    print("I didn't know you could yoodle")
+def main():
+    joke()
+
+main()
